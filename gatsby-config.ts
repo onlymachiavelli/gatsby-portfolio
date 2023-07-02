785f18f56679d26c9d239e76c6312ec80d054417
Alaa Barka <tmakaveli643@gmail.com>
2023-07-02 20:43:40 +0100

    init the configs

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,14 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `machiavelli`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Alaa Barka`,
+    siteUrl: `https://www.machiavelli.me`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
